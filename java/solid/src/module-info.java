0d02c621a4218ea764c6656779c6dd176339ebb8
Artur Oscar <Arturoscar3@gmail.com>
2021-08-25 07:20:38 -0300

    Resumo do Alura.

--- java/solid/src/module-info.java
+++ java/solid/src/module-info.java
@@ -28,6 +28,17 @@ module solid {
             * Nao faca o trabalho da classe, fora dela!
             * Qualquer mudanca em um ponto, quebra outros pontos. Mexe em A, impacta B, B impacta C
             * Se mal feito, quebra a aplicacao de forma facil.
-    *
+    *   Resumo ALURA
+    *   Coesão:
+            Uma classe coesa faz bem uma única coisa
+            Classes coesas não devem ter várias responsabilidades
+        Encapsulamento:
+            Getters e setters não são formas eficientes de aplicar encapsulamento
+            É interessante fornecer acesso apenas ao que é necessário em nossas classes
+            O encapsulamento torna o uso das nossas classes mais fácil e intuitivo
+        Acoplamento:
+            Acoplamento é a dependência entre classes
+            Acoplamento nem sempre é ruim, e que é impossível criar um sistema sem nenhum acoplamento
+            Devemos controlar o nível de acoplamento na nossa aplicação (falaremos mais sobre isso)
     * */
 }
