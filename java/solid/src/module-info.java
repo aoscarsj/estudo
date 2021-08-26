375ed8836f4e19de542f2ae74ae94382defe3d9b
Artur Oscar <Arturoscar3@gmail.com>
2021-08-26 08:07:25 -0300

    Finalizando o curso.

--- java/solid/src/module-info.java
+++ java/solid/src/module-info.java
@@ -18,7 +18,17 @@ module solid {
             *                       - Barbara Liskov
             *   Quando a heranca nao fizer sentido, usar composicao.
         * Interface Segragation Principle
+            *     As vezes sera necessario implementar uma interface que nao faz sentido.
+            *   "Uma classe nao deveria ser forcada a depender de metodos que nao utilizara."
+            *                       -Robert (Uncle Bob) Martin.
+            *   A classe as vezes tera de implementar metodos que nao fazem sentido pq da interface
+            *   O que fazer? Cria outra interface e a que usa o metodo, joga.
+            *   A classe nao deve implementar metodos que nao fazem sentido!
+            *   Joga uma heranca entre interfaces. Resolve o problema.
         * Dependency Inversion Principle
+            *   "Abstracoes nao devem depender de implementacoes
+            *    implementacoes devem depender de abstracao"
+            *                       -Robert (Uncle Bob) Martin.
     *
     * Princípios da OO
         * Coesão
@@ -42,7 +52,7 @@ module solid {
             * Nao faca o trabalho da classe, fora dela!
             * Qualquer mudanca em um ponto, quebra outros pontos. Mexe em A, impacta B, B impacta C
             * Se mal feito, quebra a aplicacao de forma facil.
-    *   Resumo ALURA
+    *   Resumo:
     *   Coesão:
             Uma classe coesa faz bem uma única coisa
             Classes coesas não devem ter várias responsabilidades
@@ -54,5 +64,24 @@ module solid {
             Acoplamento é a dependência entre classes
             Acoplamento nem sempre é ruim, e que é impossível criar um sistema sem nenhum acoplamento
             Devemos controlar o nível de acoplamento na nossa aplicação (falaremos mais sobre isso)
+    *
+    * Resumo final:
+        * Que é mais interessante e mais seguro para o nosso código depender de interfaces
+            (classes abstratas, assinaturas de métodos e interfaces em si)
+            do que das implementações de uma classe;
+        * Que as interfaces são menos propensas a sofrer mudanças enquanto implementações
+            podem mudar a qualquer momento;
+        * Que o Princípio de Inversão de Dependência (DIP) diz que implementações devem
+            depender de abstrações e abstrações não devem depender de implementações;
+        * Que as interfaces devem definir apenas os métodos que fazem sentido para seu contexto;
+        * Que o Princípio de Segregação de Interfaces (ISP) diz que uma classe não
+            deve ser obrigada a implementar um método que ela não precisa;
+        * Os conceitos aprendidos neste treinamento formam o acrônimo SOLID
+            * Single Responsibility Principle
+            * Open Closed Principle
+            * Liskov Substitution Principle
+            * Interface Segregation Principle
+            * Dependency Inversion Principle
+    *
     * */
 }
